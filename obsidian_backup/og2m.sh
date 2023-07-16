cf8880bc8972358914f6f734c43b00eb3b0ecd0a
tomtor8 <tomas.torday@gmail.com>
2023-07-16 15:55:02 +0200

    fix: obsidian backup /Users/tom variable used

--- obsidian_backup/og2m.sh
+++ obsidian_backup/og2m.sh
@@ -3,19 +3,19 @@
 # check internet connection
 if wget -q --spider https://google.com; then
 	if [ "$#" -eq 0 ]; then
-		rclone sync gdrive:Obsidian/ /Users/tomastorday/Documents/Obsidian/ -v --exclude='.obsidian/**' --exclude='.DS_Store'
+		rclone sync gdrive:Obsidian/ $HOME/Documents/Obsidian/ -v --exclude='.obsidian/**' --exclude='.DS_Store'
 		exit
 	fi
 	while getopts ':clw' OPTION; do
 		case "$OPTION" in
 		c)
-			rclone sync gdrive:Obsidian/ComTech/ /Users/tomastorday/Documents/Obsidian/ComTech/ -v --exclude='.obsidian/**' --exclude='.DS_Store'
+			rclone sync gdrive:Obsidian/ComTech/ $HOME/Documents/Obsidian/ComTech/ -v --exclude='.obsidian/**' --exclude='.DS_Store'
 			;;
 		l)
-			rclone sync gdrive:Obsidian/Learning/ /Users/tomastorday/Documents/Obsidian/Learning/ -v --exclude='.obsidian/**' --exclude='.DS_Store'
+			rclone sync gdrive:Obsidian/Learning/ $HOME/Documents/Obsidian/Learning/ -v --exclude='.obsidian/**' --exclude='.DS_Store'
 			;;
 		w)
-			rclone sync gdrive:Obsidian/Work/ /Users/tomastorday/Documents/Obsidian/Work/ -v --exclude='.obsidian/**' --exclude='.DS_Store'
+			rclone sync gdrive:Obsidian/Work/ $HOME/Documents/Obsidian/Work/ -v --exclude='.obsidian/**' --exclude='.DS_Store'
 			;;
 		*)
 			echo "Usage: $(basename "$0") [-c] for ComTech [-l] for Learning [-w] for Work."
